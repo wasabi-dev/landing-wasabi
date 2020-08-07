b4c430c7ac29636fa0aa38b75ccca5f8078075ca
La Cynty <gcbtecno@gmail.com>
2020-08-07 19:55:33 -0300

    Boton cerrar agregado

--- js/micodigo.js
+++ js/micodigo.js
@@ -1,5 +1,9 @@
 
   function OpenForm(){
-    document.getElementById("chatbox").style.display ="block";
+      document.getElementById("chatbox").style.display = "block";
+      document.getElementById("icon").style.display = "block";
   }
-  
+  function cerrarForm(){
+    document.getElementById("chatbox").style.display = "none";
+    document.getElementById("icon").style.display = "none";
+  }
