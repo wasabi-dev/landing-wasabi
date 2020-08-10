087d57492db75c5190684353b109c1f584ce4603
Cynty <gcbtecno@gmail.com>
2020-08-10 10:03:19 -0300

    cambios responsive

--- js/micodigo.js
+++ js/micodigo.js
@@ -1,9 +1,2 @@
 
-  function OpenForm(){
-      document.getElementById("chatbox").style.display = "block";
-      document.getElementById("icon").style.display = "block";
-  }
-  function cerrarForm(){
-    document.getElementById("chatbox").style.display = "none";
-    document.getElementById("icon").style.display = "none";
-  }
+  
